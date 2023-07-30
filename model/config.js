bba211b6abb0a777e7d3c909fb79f2d1a50113f9
Aflu <mohamedaflah186@gmail.com>
2023-07-30 11:00:03 +0530

    url Changed

--- model/config.js
+++ model/config.js
@@ -1,10 +1,11 @@
 const MONGO_URL =
-  "mongodb+srv://mohamedaflah186:fx7WzLg4LgP4eQEv@cluster0.2zwiyzp.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://aflah:YllR2BpiMW5WtkJB@cluster0.yunclcm.mongodb.net/?retryWrites=true&w=majority";
+// "mongodb+srv://mohamedaflah186:YKAUtFW83HUVw4pP@cluster0.2zwiyzp.mongodb.net/?retryWrites=true&w=majority"
+
 const mongoose = require("mongoose");
 mongoose.connect(MONGO_URL);
 
 mongoose.connection.on("connected", () => console.log("connected to DB"));
 mongoose.connection.on("disconnected", () => console.log("disconnected to DB"));
 
-// fx7WzLg4LgP4eQEv
-// mohamedaflah186
+// YllR2BpiMW5WtkJB
